837619db8b79bce65feb6e62d4e40f05e11474f3
hypn0x <leetpwnsroot@gmail.com>
2022-03-04 12:28:49 +0100

    module02

---
+++ module02/ex00/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @author      : hypn0x (wubbalubba@dubdub)
+ * @file        : main
+ * @created     : Friday Mar 04, 2022 11:52:39 CET
+ */
+
+#include <iostream>
+
+int main()
+{
+    
+    return 0;
+}
+
