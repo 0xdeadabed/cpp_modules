89b6471303a19df11aa1d8c5639b2f07248c6d19
hypn0x <leetpwnsroot@gmail.com>
2022-03-07 11:25:48 +0100

    Starting m03

---
+++ module03/ex00/ClapTrap.hpp
@@ -0,0 +1,35 @@
+/**
+ * @author      : hypn0x (wubbalubba@dubdub)
+ * @file        : ClapTrap
+ * @created     : Monday Mar 07, 2022 10:30:41 CET
+ */
+
+#ifndef CLAPTRAP_HPP
+
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+    private:
+        /* private data */
+		std::string _name;
+		unsigned int _hitPoint;
+		unsigned int _energyPoint;
+		unsigned int _attackDamage;
+    public:
+        ClapTrap ();
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &other);
+        ~ClapTrap ();
+
+		ClapTrap &operator=(const ClapTrap &other);
+
+		void attack(const std::string &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
+
+#endif /* end of include guard CLAPTRAP_HPP */
+
